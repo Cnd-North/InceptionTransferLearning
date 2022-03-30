736a9f7b76179cb40285c8626b18501c1e1bd20e
Cnd-North <gerrit.vr12@gmail.com>
2022-03-29 20:45:28 -0700

    Creating file to transfer learn InceptionV3

---
+++ # Inception Transfer Learning.py
@@ -0,0 +1,9 @@
+# Inception Transfer Learning
+# Gerrit van Rensburg
+# 2022-03-29
+# Followed instructions from Tekan Irla - https://medium.com/analytics-vidhya/transfer-learning-using-inception-v3-for-image-classification-86700411251b
+
+
+# Import that inception model
+from tensorflow.keras.application.inception_v3 import InceptionV3
+
