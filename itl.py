afe2ad4f93332073fc4bf0d59d33ad526aa9d1a6
Cnd-North <gerrit.vr12@gmail.com>
2022-04-01 20:31:12 -0700

    Updated lines 27,30,33,36 from layers to tf.keras.layers && line 38 tf.keras.models.

--- itl.py
+++ itl.py
@@ -6,6 +6,7 @@
 
 # Import that inception model
 from re import X
+import tensorflow as tf
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 
 pre_trained_model = InceptionV3(input_shape = (150, 150, 3),   # Shape of the images
@@ -23,18 +24,18 @@ for layer in pre_trained_model.layers:
 from tensorflow.keras.optimizers import RMSprop
 
 # Flatten the output layer to 1 dimension
-x = layers.Flatten()(pre_trained_model.output)
+x = tf.keras.layers.Flatten()(pre_trained_model.output)
 
 # Add a fully connected layer with 1,024 hidden units and ReLU activation
-x = layers.Dense(1024, activation='relu')(x)
+x = tf.keras.layers.Dense(1024, activation='relu')(x)
 
 # Add a dropout rate of 0.2
-x = layers.Dropout(0.2)(x)
+x = tf.keras.layers.Dropout(0.2)(x)
 
 # Add a final sigmoid layer for classification
-x = layers.Dense (1, activation='sigmoid')(x)
+x = tf.keras.layers.Dense (1, activation='sigmoid')(x)
 
-model = Model( pre_trained_model.input, x)
+model = tf.keras.models.Model( pre_trained_model.input, x)
 
 model.compile(optimizer = RMSprop(lr=0.0001),
             loss = 'binary_crossentropy',
