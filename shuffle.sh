553fadf5e3377fca451ebe24371e4822c0460377
Cnd-North <gerrit.vr12@gmail.com>
2022-04-17 10:43:39 -0700

    updated shuf to gshuf

--- shuffle.sh
+++ shuffle.sh
@@ -5,5 +5,5 @@
 
 # current ~42,000 images were generated & 20% (8400) will be used for validation
 
-ls | shuf -n 8400 | xargs -i mv {} path-to-new-folder
+ls | gshuf -n 8400 | xargs -I mv {} path-to-new-folder
 
